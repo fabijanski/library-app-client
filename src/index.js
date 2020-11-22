8c026ac7d77739df29b9badac05def8f5cfe301e
Michał Fabijański <mm.fabijanski@gmail.com>
2020-11-22 20:33:09 +0100

    Add prettier config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { ApolloProvider } from "@apollo/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
+import App from './App'
 
-const GRAPHQL_ENDPOINT = "https://serene-tundra-45425.herokuapp.com/graphql";
+const GRAPHQL_ENDPOINT = 'https://serene-tundra-45425.herokuapp.com/graphql'
 
 const cache = new InMemoryCache({
   addTypename: false,
-  resultCaching: false
-});
+  resultCaching: false,
+})
 const client = new ApolloClient({
   uri: GRAPHQL_ENDPOINT,
-  cache
-});
+  cache,
+})
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root')
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  rootElement
-);
+  rootElement,
+)
